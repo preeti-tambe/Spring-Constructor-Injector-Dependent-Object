cac7f10685c216b37311d519e9a4420eff72d835
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-03-25 12:51:33 +0530

    Created apalication context xml file

--- src/model/Address.java
+++ src/model/Address.java
@@ -20,6 +20,13 @@ public class Address {
 	public String toString() {
 		return "Address [city=" + city + ", state=" + state + ", country=" + country + "]";
 	}
+
+	public Address(String city, String state, String country) {
+		super();
+		this.city = city;
+		this.state = state;
+		this.country = country;
+	}
 	 
 	 
 
