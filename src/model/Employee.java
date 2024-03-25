0d30f7cb0d0e79aa36e538c5af154925bcdb83d7
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-03-25 12:47:12 +0530

    Created Employee Class

---
+++ src/model/Employee.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Employee {
+
+	private int id;  
+	private String name;  
+	private Address address;
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Employee(int id, String name, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	
+	void show(){  
+	    System.out.println(id+" "+name);  
+	    System.out.println(address.toString());  
+	}  
+	  
+	
+	
+	
+}
